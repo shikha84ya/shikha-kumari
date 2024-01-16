d037ce690da382f1d202ce60cbc0a36e32f8ac34
shikha84ya <156786332+shikha84ya@users.noreply.github.com>
2024-01-16 15:31:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,4 @@ if st.button('Predict'):
     predicted_class = trained_model.predict(input_features_array)
 
 
-    
-    st.write(f"Predicted class: {predicted_class[0]}")
-if predicted_class[0]== 1:
-    print("you will survive")
-else:
-    print("you will not survive")
        
